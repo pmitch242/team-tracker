5c6bf36491094a7dedff7cd69a4e542515c22f3d
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-25 09:14:36 -0500

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO departments(name)
+	VALUES ("Engineering"), ("Accounting");
+    
+INSERT INTO roles(title, salary)
+	VALUES ('Project Engineer', 110000),
+    ('Business Analyst', 86000),
+    ('Feild Engineer', 87000);
+    
+INSERT INTO employees(first_name, last_name)
+	VALUES ('Renee', 'Mitchell-Matsuyama'),
+    ('Phill', 'Mitchell-Matsuyama'),
+    ('Starfish', 'Turgeon'),
+    ('Jay','Elkino');
+    
+    SELECT * FROM roles; 
+SELECT * FROM employees;
+SELECT * FROM departments;
