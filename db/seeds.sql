8b71b8b75c67973e8699a24c0a0a6d60c570cd94
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-12 16:41:36 -0500

    added seed for testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,19 @@
 INSERT INTO departments(name)
 	VALUES ("Engineering"), ("Accounting");
     
-INSERT INTO roles(title, salary)
-	VALUES ('Project Engineer', 110000),
-    ('Business Analyst', 86000),
-    ('Feild Engineer', 87000);
+INSERT INTO roles(title, salary, depart_id)
+	VALUES ('Project Engineer', 110000, 1),
+    ('Business Analyst', 86000, 2),
+    ('Feild Engineer', 87000, 1);
     
-INSERT INTO employees(first_name, last_name)
-	VALUES ('Renee', 'Mitchell-Matsuyama'),
-    ('Phill', 'Mitchell-Matsuyama'),
-    ('Starfish', 'Turgeon'),
-    ('Jay','Elkino');
-    
-    SELECT * FROM roles; 
+INSERT INTO employees(first_name, last_name, role_id)
+	VALUES ('Renee', 'Mitchell-Matsuyama', 1),
+    ('Phill', 'Mitchell-Matsuyama', 3),
+    ('Starfish', 'Turgeon', 2),
+    ('Jay','Elkino', 3);
+   
+   
+SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.name FROM employees INNER JOIN roles ON (employees.role_id = roles.id) INNER JOIN departments ON (roles.depart_id = departments.id);
+SELECT * FROM roles; 
 SELECT * FROM employees;
 SELECT * FROM departments;
